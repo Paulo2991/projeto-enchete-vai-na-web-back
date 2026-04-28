513a6f0d3e1d408532924145e1fe64fbbb03570d
Paulo2991 <paulovaz38034@gmail.com>
2026-04-28 20:56:27 -0300

    Adicionando try-catch para verificar deploy

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
-const app = require("./src/app");
+try {
+  require("./server");
+  const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0',() => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+} catch (error) {
+  console.error("ERRO CRÍTICO NA INICIALIZAÇÃO:", error);
+}
